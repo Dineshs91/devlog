3290733701c2b4e029efa4dda50fa66f23f95d8e
dinesh <dsdiscoverdinesh@gmail.com>
2015-02-22 00:21:47 +0530

    Add jshint

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,8 +4,16 @@ module.exports = function(grunt) {
     if (grunt.file.exists('tasks')) {
         grunt.loadTasks('tasks');
     }
+    
+    grunt.loadNpmTasks('grunt-contrib-jshint');
 
     grunt.registerTask('setup', [
        'e2e-setup'
     ]);
+    
+    grunt.initConfig({
+        jshint: {
+            all: ['app/js/**/*.js', 'test/unit/**/*.js', 'test/e2e/**/*.js']
+        } 
+    });
 }
