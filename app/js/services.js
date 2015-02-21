ecbe594855461ed90bfa35c9ccf5070171c75496
dinesh <dsdiscoverdinesh@gmail.com>
2015-02-22 01:35:09 +0530

    Add dbService and corresponding tests

--- app/js/services.js
+++ app/js/services.js
@@ -1,3 +1,147 @@
-devlog.service('dbService', ['$q', function($q) {
+var path = require('path');
+var gui = require('nw.gui');
+var App = gui.App;
+var Datastore = require('nedb');
+var db = new Datastore({
+    filename: path.join(App.dataPath, 'db/log.db'),
+    autoload: true
+});
+
+devlog.service('dbService', ['$q', '$rootScope', function($q, $rootScope) {
     
+    /*
+        Convert nedb's _id to key to be used in the view.
+        nedb still uses _id.
+    */
+    var convertIdToKey = function(logs) {
+        var convertedLogs = [];
+        var logLength = logs.length;
+        
+        for (i = 0; i < logLength; i++) {
+            var log = logs[i];
+            convertedLog = {};
+            for ( var ele in log) {
+                if(!log.hasOwnProperty(ele)) {
+                    continue;
+                }
+                if(ele === '_id') {
+                    convertedLog.key = log[ele];
+                } else {
+                    convertedLog[ele] = log[ele];
+                }
+            }
+
+            convertedLogs.push(convertedLog);
+        }
+
+        return convertedLogs;
+    };
+    
+    this.getLog = function(key) {
+        var deferred = $q.defer();
+
+        db.findOne({_id: key}, function(err, doc) {
+            if(!err) {
+                doc = convertIdToKey([doc])[0];
+                deferred.resolve(doc);
+            } else {
+                deferred.reject(err);
+            }
+            
+            $rootScope.$apply();
+        });
+
+        return deferred.promise;
+    };
+ 
+    this.getLogs = function() {
+        var deferred = $q.defer();
+
+        db.find({}, function(err, docs) {
+            if(!err) {
+                docs = convertIdToKey(docs);
+                deferred.resolve(docs);
+            } else {
+                deferred.reject(err);
+            }
+            
+            $rootScope.$apply();
+        });
+
+        return deferred.promise;
+    };
+
+    this.insertLog = function(doc) {
+        var deferred = $q.defer();
+
+        db.insert(doc, function(err, newDoc) {
+            if(!err) {
+                deferred.resolve(newDoc);
+            } else {
+                deferred.reject(err);
+            }
+            
+            $rootScope.$apply();
+        });
+
+        return deferred.promise;
+    };
+
+    this.updateLog = function(doc) {
+        var deferred = $q.defer();
+
+        db.update({_id: doc.key}, {$set: {content: doc.content, title: doc.title, timestamp: doc.timestamp, tags: doc.tags}}, {},
+        function(err) {
+            if(!err) {
+                deferred.resolve();
+            } else {
+                deferred.reject(err);
+            }
+            
+            $rootScope.$apply();
+        });
+
+        return deferred.promise;
+    };
+
+    /*
+        Toggle the boolean variable is_removed.
+    */
+    this.removeLog = function(key) {
+        var deferred = $q.defer();
+
+        db.update({_id: key}, {$set: {is_removed: true}}, function(err) {
+            if(!err) {
+                deferred.resolve();
+            } else {
+                deferred.reject(err);
+            }
+            
+            $rootScope.$apply();
+        });
+
+        return deferred.promise;
+    };
+
+    /*
+        Permanently delete log.
+        
+        Warning:  Using this method will delete log permanently. There
+        is no way, to recover the deleted data, from the app.
+    */
+    this.permanentDelete = function(key) {
+        var deferred = $q.defer();
+
+        db.remove({_id: key}, function(err) {
+            if(!err) {
+                deferred.resolve();
+            } else {
+                deferred.reject(err);
+            }
+
+            $rootScope.$apply();
+        });
+
+        return deferred.promise;
+    };
 }]);
--- test/unit/servicesSpec.js
+++ test/unit/servicesSpec.js
@@ -1,5 +1,23 @@
 describe("DevLog Services", function() {
     var dbService;
+    var testKey;
+
+    var testDoc = {
+        'title': 'Test',
+        'content': 'Test content',
+        'timestamp': '1424546852',
+        'is_removed': false,
+        'tags': ['test']
+    };
+
+    var updateDoc = {
+        'title': 'Test update',
+        'content': 'Test updated content',
+        'timestamp': '1424546854',
+        'is_removed': false,
+        'tags': ['test', 'update']
+    };
+
     beforeEach(module('devLog'));
 
     beforeEach(inject(function(_dbService_) {
@@ -7,6 +25,99 @@ describe("DevLog Services", function() {
     }));
 
     it('should contain a dbService', function() {
-        expect(dbService).not.toBe(null);
+        expect(dbService).toBeDefined();
     });
+
+    it('should insert a log', function(done) {
+        var promise = dbService.insertLog(testDoc);
+        
+        promise.then(function(newDoc) {
+            testKey = newDoc._id;
+            done();
+        });
+        
+        expect(promise).toBeDefined();
+    });
+
+    it('should get all the logs', function(done) {
+        var promise = dbService.getLogs();
+
+        promise.then(function(logs) {
+            expect(logs).toBeDefined();
+            expect(logs.length).toBe(1);
+            var log = logs[0];
+            
+            expect(log.title).toBe(testDoc.title);
+            expect(log.content).toBe(testDoc.content);
+            expect(log.tags[0]).toBe(testDoc.tags[0]);
+            
+            // Test if _id is converted to key
+            expect(log.key).toBeDefined();
+            expect(log._id).toBeUndefined();
+            done();
+        });
+    });
+    
+    it('should get a log', function(done) {
+        var promise = dbService.getLog(testKey);
+
+        promise.then(function(log) {
+            expect(log).toBeDefined();
+            expect(log.title).toBe(testDoc.title);
+            expect(log.content).toBe(testDoc.content);
+            expect(log.tags.length).toBe(testDoc.tags.length);
+            expect(log.tags[0]).toBe(testDoc.tags[0]);
+            expect(log.timestamp).toBe(testDoc.timestamp);
+            
+            // Test if _id is converted to key
+            expect(log.key).toBeDefined();
+            expect(log._id).toBeUndefined();
+            done();
+        });
+    });
+
+    it('should update log', function(done) {
+        // Add key to updateDoc
+        updateDoc.key = testKey;
+        var promise = dbService.updateLog(updateDoc);
+        var getLogPromise = dbService.getLog(testKey);
+        
+        promise.then(function() {
+            return getLogPromise;
+        }).then(function(log) {
+            expect(log.title).toBe(updateDoc.title);
+            expect(log.content).toBe(updateDoc.content);
+            expect(log.tags.length).toBe(updateDoc.tags.length);
+            for (var i = 0; i < log.tags.length; i++) {
+                expect(log.tags[i]).toBe(updateDoc.tags[i]);
+            }
+            expect(log.timestamp).toBe(updateDoc.timestamp);
+            done();
+        });
+    });
+    
+    it('should remove a log', function(done) {
+        var promise = dbService.removeLog(testKey);
+        var getLogPromise = dbService.getLog(testKey);
+        
+        promise.then(function() {
+            return getLogPromise;
+        }).then(function(log) {
+            expect(log.is_removed).toBe(true);
+            done();
+        });
+    });
+    
+    it('should remove log permanently', function(done) {
+        var promise = dbService.permanentDelete(testKey);
+        var getAllLogsPromise = dbService.getLogs();
+
+        promise.then(function() {
+            return getAllLogsPromise;
+        }).then(function(logs) {
+            expect(logs.length).toBe(0);
+            done();
+        });
+    });
+    
 });
