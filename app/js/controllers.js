5c55877fcf8527610768395f87f8eff80d504dbf
dinesh <dsdiscoverdinesh@gmail.com>
2015-02-24 23:05:55 +0530

    Add db service mock

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,5 +1,10 @@
 var devlog = angular.module('devLog', []);
 
-devlog.controller('LogCtrl', ['$scope', function($scope) {
+devlog.controller('LogCtrl', ['$scope', 'dbService', function($scope, dbService) {
+    $scope.getAll = function() {
+        dbService.getAllLogs().then(function(logs) {
+            $scope.logs = logs;
+        });
+    };
     
 }]);
--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -1,13 +1,189 @@
 describe("DevLog Controllers", function() {
-    var scope, logCtrl;
-    beforeEach(module('devLog'));
-
-    beforeEach(inject(function($rootScope, $controller) {
-        scope = $rootScope.$new();
-        logCtrl = $controller('LogCtrl', {$scope: scope});
+    var $scope, logCtrl, dbService;
+    
+    beforeEach(function() {
+        var mockDbService = {};
+        module('devLog', function($provide) {
+            $provide.value('dbService', mockDbService);
+        });
+        
+        inject(function($q) {
+            mockDbService.logs = [
+                {
+                    'key': '2VUEDrzhTHPOVxys',
+                    'title': 'test title',
+                    'content': 'test content',
+                    'timestamp': '1424546852',
+                    'is_removed': false,
+                    'tags': ['test']
+                },
+                {
+                    'key': '64VdkAI6C80FUuxk',
+                    'title': 'Test 2',
+                    'content': 'Test content 2',
+                    'timestamp': '1424546853',
+                    'is_removed': false,
+                    'tags': ['update']
+                }
+            ];
+            
+            mockDbService.tags = [
+                {
+                    '_id': '442477',
+                    'key': 'wrmvg5c2ZuzGLxbh',
+                    'tag': 'test'
+                },
+                {
+                    '_id': '197223',
+                    'key': 'rtYsgTmnaQz9Ldbv',
+                    'tag': 'update'
+                }
+            ];
+            
+            var getRandomInt = function() {
+                var min = 100000;
+                var max = 999999;
+                return Math.floor(Math.random() * (max - min + 1)) + min;
+            };
+            
+            mockDbService.getLog = function(key) {
+                var deferred = $q.defer();
+                
+                for (var log in this.logs) {
+                    if(log.key === key) {
+                        deferred.resolve(log);
+                    }
+                }                
+                
+                return deferred.promise;
+            };
+            
+            mockDbService.getAllLogs = function() {
+                var deferred = $q.defer();
+                
+                deferred.resolve(this.logs);
+                
+                return deferred.promise;
+            };
+            
+            mockDbService.getLogsWithTag = function(tag) {
+                var deferred = $q.defer();
+                var logsWithTag = [];
+                
+                for (var log in this.logs) {
+                    if(log.tags[0] === tag) {
+                        logsWithTag.push(log);
+                    }
+                }
+                deferred.resolve(logsWithTag);
+                
+                return deferred.promise;
+            };
+            
+            mockDbService.insertLog = function(log) {
+                var deferred = $q.defer();
+                
+                // Create some random key.
+                log.key = getRandomInt;
+                this.logs.push(log);
+                
+                deferred.resolve();
+                
+                return deferred.promise;
+            };
+            
+            mockDbService.updateLog = function(updateLog) {
+                var deferred = $q.defer();
+                
+                for (var log in this.logs) {
+                    if(updateLog.key === log.key) {
+                        log.title = updateLog.title;
+                        log.content = updateLog.content;
+                        log.timestamp = updateLog.timestamp;
+                        log.tags = updateLog.tags;
+                    }
+                }                
+                deferred.resolve();
+                
+                return deferred.promise;
+            };
+            
+            mockDbService.removeLog = function(key) {
+                var deferred = $q.defer();
+                
+                for (var log in this.logs) {
+                    if(log.key === key) {
+                        log.is_removed = true;
+                    }
+                }
+                deferred.resolve();
+                
+                return deferred.promise; 
+            };
+            
+            mockDbService.permanentDelete = function(key) {
+                var deferred = $q.defer();
+                
+                for (var i = 0; i < this.logs.length; i++) {
+                    var log = this.logs[i];
+                    if(log.key === key) {
+                        logs.splice(i, 1);
+                    }
+                }
+                deferred.resolve();
+                
+                return deferred.promise;
+            };
+            
+            mockDbService.insertTag = function(tag) {
+                var deferred = $q.defer();
+                
+                tag.key = getRandomInt;
+                this.tags.push(tag);
+                
+                deferred.resolve();
+                
+                return deferred.promise;
+            };
+            
+            mockDbService.removeTag = function(tagName) {
+                var deferred = $q.defer();
+                
+                for(var i = 0; i < this.tags.length; i++) {
+                    var tag = this.tags[i];
+                    if(tag.tag === tagName) {
+                        tag.splice(i, 1);
+                    }
+                }
+                
+                deferred.resolve();
+                
+                return deferred.promise;
+            };
+            
+            mockDbService.getAllTags = function() {
+                var deferred = $q.defer();
+                
+                deferred.resolve(this.tags);
+                
+                return deferred.promise;
+            };
+        });
+    });
+    
+    beforeEach(inject(function($controller, $rootScope, _dbService_) {
+        $scope = $rootScope.$new();
+        dbService = _dbService_;
+        logCtrl = $controller('LogCtrl', {$scope: $scope, dbService: dbService});  
     }));
 
     it('should have a LogCtrl controller', function() {
         expect(logCtrl).not.toBe(null);
+    });
+    
+    it('should get all logs', function() {
+        $scope.getAll();
+        $scope.$apply();
+        expect($scope.logs.length).toBe(2); 
     });
 });
