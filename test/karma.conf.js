8f2c0529b6218d501cb9de802ec6ce39bb7df140
dinesh <dsdiscoverdinesh@gmail.com>
2015-03-06 19:51:54 +0530

    Go back to text-summary for coverage

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -33,7 +33,7 @@ module.exports = function(config) {
         },
 
         coverageReporter: {
-            type: 'html',
+            type: 'text-summary',
             dir: 'test/coverage/'
         },
 
