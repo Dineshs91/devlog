4a66960d519c19efcb2f2282e856e8764b6cc48d
dinesh <dsdiscoverdinesh@gmail.com>
2015-03-22 23:34:05 +0530

    Move db connection code to db.js
    db is injected as a dependency to the dbService

--- app/js/services.js
+++ app/js/services.js
@@ -1,39 +1,7 @@
-var path = require('path');
-var gui = require('nw.gui');
-var App = gui.App;
-var Datastore = require('nedb');
-var db = {};
-
-/*
-    db that holds the logs.
-*/
-db.logs = new Datastore({
-    filename: path.join(App.dataPath, 'db/log.db'),
-    autoload: true
-});
-
-/*
-    db that holds only the unique tag names.
-*/
-db.tags = new Datastore({
-    filename: path.join(App.dataPath, 'db/tag.db'),
-    autoload: true
-});
-
-/*
-    Enforce an unique constraint on tag.
-*/
-db.tags.ensureIndex({ fieldName: 'tag', unique: true }, function(err) {
-    if(err) {
-        console.log(err);
-    }
-});
-
 /*
     An angular service, for all db related transactions.
 */
-devlog.service('dbService', ['$q', '$rootScope', function($q, $rootScope) {
-    
+devlog.service('dbService', ['$q', '$rootScope', 'db', function($q, $rootScope, db) {
     /*
         Convert nedb's _id to key to be used in the view.
         nedb still uses _id.
