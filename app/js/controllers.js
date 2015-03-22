3d36c57e9d866c1ab762aa3607461fa841804af3
dinesh <dsdiscoverdinesh@gmail.com>
2015-03-22 23:32:25 +0530

    Add logic for checking purpose

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,10 +1,12 @@
 var devlog = angular.module('devLog', []);
 
 devlog.controller('LogCtrl', ['$scope', 'dbService', function($scope, dbService) {
+
     $scope.getAll = function() {
         dbService.getAllLogs().then(function(logs) {
             $scope.logs = logs;
         });
     };
     
+    $scope.getAll();
 }]);
