570602f2adc815c4d069cd183d65eb0d79160f0f
dinesh <dsdiscoverdinesh@gmail.com>
2015-04-11 15:18:30 +0530

    Add nodewebkit builder task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,44 @@
 'use strict';
 
+var pkg = require("./package.json");
+
+var NW_VERSION = "0.11.6";
+
 module.exports = function(grunt) {
     if (grunt.file.exists('tasks')) {
         grunt.loadTasks('tasks');
     }
     
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-node-webkit-builder');
 
+    grunt.initConfig({
+        pkg: pkg,
+        jshint: {
+            all: ['app/js/**/*.js', 'test/unit/**/*.js', 'test/e2e/**/*.js']
+        },
+    	nodewebkit: {
+	        options: {
+                appName: 'Devlog',
+                appVersion: pkg.version,
+	            platforms: ['osx64', 'linux64', 'win64'],
+	            buildDir: './build',
+                version: NW_VERSION,
+                zip: false
+	        },
+	        src: [
+                './**/*',
+                '!./test/**',
+                '!./node_modules/**',
+                '!./test_out/**',
+                '!./cache/**'
+            ]
+    	},
+    });
+    
     grunt.registerTask('setup', [
        'e2e-setup'
     ]);
     
-    grunt.initConfig({
-        jshint: {
-            all: ['app/js/**/*.js', 'test/unit/**/*.js', 'test/e2e/**/*.js']
-        } 
-    });
+    grunt.registerTask('default', ['nodewebkit']);
 }
