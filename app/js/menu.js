b35d0bf7118f843add2e865abdb9828268b9f777
dinesh <dsdiscoverdinesh@gmail.com>
2015-04-13 00:21:18 +0530

    Add an options menu
    add restore/delete menu item

--- app/js/menu.js
+++ app/js/menu.js
@@ -1,7 +1,21 @@
 var gui = require('nw.gui');
 var win = gui.Window.get();
-var nativeMenuBar = new gui.Menu({ type: "menubar" });
+var menu = new gui.Menu({ type: "menubar" });
 
-if(process.platform === 'darwin') nativeMenuBar.createMacBuiltin("Devlog");
+if(process.platform === 'darwin') menu.createMacBuiltin("Devlog");
 
-win.menu = nativeMenuBar;
+var optionsMenu = new gui.Menu();
+
+optionsMenu.append(new gui.MenuItem({
+    label: "restore/delete",
+    click: function() {
+        $('.standard.modal').modal('show');
+    }
+}));
+
+menu.append(new gui.MenuItem({
+    label: 'Options',
+    submenu: optionsMenu
+}));
+
+win.menu = menu;
