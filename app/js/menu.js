49fdc906ab90e3da327ba6cbabaeba8edb433259
dinesh <dsdiscoverdinesh@gmail.com>
2015-04-13 23:19:44 +0530

    Add key bindings for new and add log
    Add a new option named logs.
    Add key binding for restore/delete log

--- app/js/menu.js
+++ app/js/menu.js
@@ -8,9 +8,36 @@ var optionsMenu = new gui.Menu();
 
 optionsMenu.append(new gui.MenuItem({
     label: "restore/delete",
+    key: "r",
+    modifiers: process.platform === 'darwin' ? "cmd" : "ctrl",
     click: function() {
         $('.standard.modal').modal('show');
     }
+}));
+
+var logsMenu = new gui.Menu();
+
+logsMenu.append(new gui.MenuItem({
+    label: "Add new log",
+    key: "n",
+    modifiers: process.platform === 'darwin' ? "cmd" : "ctrl",
+    click: function() {
+        $('.add').click();
+    }
+}));
+
+logsMenu.append(new gui.MenuItem({
+    label: "Save",
+    key: "s",
+    modifiers: process.platform === 'darwin' ? "cmd" : "ctrl",
+    click: function() {
+        $('.save').click();
+    }
+}));
+
+menu.append(new gui.MenuItem({
+    label: 'Logs',
+    submenu: logsMenu
 }));
 
 menu.append(new gui.MenuItem({
