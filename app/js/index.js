74b0a7e7e276dd0925c78e07704729647c326da6
dinesh <dineshpy07@gmail.com>
2015-09-02 19:07:07 +0530

    Focus on title when add is clicked

--- app/js/index.js
+++ app/js/index.js
@@ -2,4 +2,8 @@ $(function() {
     $('.deny.button').click(function() {
         $('input:radio').prop('checked', false);
     });
+
+    $('.add').click(function() {
+        $('.title').focus();
+    });
 });
