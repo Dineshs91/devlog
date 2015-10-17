0cf1ec6415035e063a56b8e5d4bd14ec06f1c329
dinesh <dineshpy07@gmail.com>
2015-10-17 21:45:02 +0530

    Add remaining test cases

--- test/e2e/pages/primary.page.js
+++ test/e2e/pages/primary.page.js
@@ -4,17 +4,32 @@ var PrimaryPage = function() {
     var tagsInput = element(by.css('input.tags'));
     var contentInput = element(by.css('textarea.content'));
     var logs = element.all(by.repeater('log in logs'));
+    var tagsNav = element.all(by.css('.tags-nav'));
+    var trashIcon = element(by.css('.trash'));
+    var trashAll = element.all(by.css('.trash'));
     
     this.clickAddButton = function() {
         addLog.click();
+    };
+    
+    this.getTitle = function() {
+        return titleInput;
     };
     
     this.setTitle = function(title) {
         titleInput.sendKeys(title);
     };
     
+    this.getTags = function() {
+        return tagsInput;
+    };
+    
     this.setTags = function(tags) {
         tagsInput.sendKeys(tags);
+    };
+    
+    this.getContentInput = function() {
+        return contentInput;
     };
     
     this.setContent = function(content) {
@@ -23,6 +38,18 @@ var PrimaryPage = function() {
     
     this.getLogCount = function() {
         return logs.count();
+    };
+    
+    this.getTagsNav = function() {
+        return tagsNav;
+    };
+    
+    this.getTrashIcon = function() {
+        return trashIcon;
+    };
+    
+    this.getTrashAll = function() {
+        return trashAll;
     };
 };
 
--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -45,4 +45,34 @@ describe('Devlog', function() {
 
         expect(page.getLogCount()).toEqual(2);
     });
+    
+    it('should get all logs in test tag', function() {
+        page.getTagsNav().get(2).click();
+
+        expect(page.getLogCount()).toEqual(1);
+        expect(page.getTitle().getAttribute('value')).toEqual(log2.title);
+        expect(page.getTags().getAttribute('value')).toEqual(log2.tags);
+        expect(page.getContentInput().getAttribute('value')).toEqual(log2.content);
+    });
+    
+    it('should remove log2', function() {
+        page.getTrashIcon().click();
+        browser.sleep(100);
+        
+        var allTag = page.getTagsNav().get(0);
+        expect(hasClass(allTag, 'active')).toBe(true);
+    });
+
+    it('should remove all logs', function() {
+        page.getTagsNav().get(0).click();
+
+        page.getTrashAll().then(function(items) {
+            for(var i = 0; i < items.length; i++) {
+                page.getTrashAll().get(0).click();
+                browser.sleep(200);
+            }
+        });
+
+        expect(page.getLogCount()).toEqual(0);
+    });
 });
