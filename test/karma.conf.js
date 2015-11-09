e305d1c2b8a38694b72aef5f0bf8f12fc2edc8ae
dinesh <dineshpy07@gmail.com>
2015-11-09 19:14:07 +0530

    Create modules directory and move module into it

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -7,7 +7,7 @@ module.exports = function(config) {
             'app/bower_components/angular/angular.js',
             'app/bower_components/angular-mocks/angular-mocks.js',
             'app/bower_components/angular-hotkeys/build/hotkeys.min.js',
-            'app/js/controllers/controllers.js',
+            'app/js/modules/BaseModule.js',
             'app/js/**/*.js',
             'test/unit/**/*.js'
         ],
